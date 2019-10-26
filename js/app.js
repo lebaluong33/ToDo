ad3bd476e2c4e4ffb3e27656c1dbadc19c0c3f45
lebaluong33 <lebaluong33@gmail.com>
2019-10-26 21:56:26 +0700

    Enable edit todo content, check all button

--- js/app.js
+++ js/app.js
@@ -1,34 +1,35 @@
-const clear = document.querySelector(".clear");
+const clear = document.getElementsByClassName("clear");
 const date = document.getElementById("date");
 const input = document.getElementById("input");
 const list = document.getElementById("list");
+const changeContent = document.getElementsByClassName("text");
+const selectAll = document.getElementById("select-all");
 
 let listToDo = [];
 let id=0;
 
-const check = "fa-circle";
-const uncheck = "fa-check-circle";
+const uncheck = "fa-circle";
+const check = "fa-check-circle";
 const lineThrough = "line-through";
 
 const options = { weekday: "long", day: "numeric", month: "short", year: "numeric"};
 const today = new Date();
-
 date.innerHTML = today.toLocaleDateString("en-US",options);
 
 const addToDo = (todo, id, done, trash) => {
   if(trash) { return; }
-  const finish = done ? uncheck : check;
+  const finish = done ? check : uncheck;
   const textDecoration = done ? lineThrough : "";
   const text = ` 
             <li class="item flex-row">
               <i class="far ${finish} complete icon-circle" job="complete" id="${id}"></i>
-              <p class=" ${textDecoration} text">${todo}</p>
+              <input class="${textDecoration} text" disabled value="${todo}" autofocus id="${id}"/>
               <i class="fas fa-trash-alt delete icon-trash" job="delete" id="${id}"></i>
             </li> ` ;
   const position = "beforeend";
   list.insertAdjacentHTML(position, text);
 };
-document.addEventListener("keyup", (event) => {
+input.addEventListener("keyup", (event) => {
   if (event.keyCode == 13) {
     const toDo = input.value;
     if (toDo) {
@@ -48,7 +49,7 @@ const completeToDo = (element) => {
   element.classList.toggle(check);
   element.classList.toggle(uncheck);
   element.parentNode.querySelector(".text").classList.toggle(lineThrough);
-  listToDo[element.id].done = listToDo[element.id].done ? false : true ;
+  listToDo[element.id].done = !listToDo[element.id].done;
 };
 const removeToDo = (element) => {
   element.parentNode.parentNode.removeChild(element.parentNode);
@@ -57,10 +58,44 @@ const removeToDo = (element) => {
 list.addEventListener("click", (event) => {
   let element = event.target;
   const elementJob = element.attributes.job.value;
-  if(elementJob == "complete"){
-    completeToDo(element);
+  switch (elementJob){
+    case "complete" :
+      completeToDo(element);
+      break;
+    case "delete":
+      removeToDo(element);
+      break;
+    default: break;
   }
-  else {
-    removeToDo(element);
+});
+list.addEventListener("dblclick", (event) => {
+  let element = event.target;
+  const elementJob = element.attributes.job.value;
+  element.toggleAttribute("disabled");
+  if(elementJob == "change") onChangeToDo(element);
+});
+const onChangeToDo = (element) => {
+  element.addEventListener("keyup", (event) => {
+    if (event.keyCode == 13) {
+      element.toggleAttribute("disabled");
+      const toDo = element.value;
+      let id = element.attributes.id.value;
+      listToDo[id].name = toDo;
+    }
+  });
+};
+const onChangeClass = ( changeIconClass, finish, doing) =>{
+  changeIconClass.remove(finish);
+  changeIconClass.add(doing);
+}; 
+selectAll.addEventListener("click", () => {
+  const textDecoration = lineThrough ;
+  for (item of listToDo){
+    item.done = true;
+  }
+  for (item of list.children){
+    const changeIconClass = item.firstElementChild.classList;
+    const hasCheck = changeIconClass.contains(check)
+    hasCheck ? onChangeClass(changeIconClass, check, uncheck) : onChangeClass(changeIconClass, uncheck,check);
   }
 });
